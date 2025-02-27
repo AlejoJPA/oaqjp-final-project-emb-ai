8609205c8c04506353c1b0fc965aed2e098dbf72
Alejo <alejojpa2@gmail.com>
2025-02-27 11:51:11 -0500

    emotions extraction

--- emotion_detection.py
+++ emotion_detection.py
@@ -18,6 +18,14 @@ def emotion_detector(text_to_analyze):
     #Headers required for the API request
     header = { "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock" }
 
+    # Initializin default values
+    anger_score = None
+    disgust_score = None
+    fear_score = None
+    joy_score = None
+    sadness_score = None
+    dominant_emotion = None
+
     try:
         #POST request to the API
         response = requests.post(url, json=myobj, headers=header, timeout=10)
@@ -25,7 +33,24 @@ def emotion_detector(text_to_analyze):
         #Parsed JSON response
         formatted_response= json.loads(response.text)
 
+        #Extracting emotions
+        if response.status_code == 200:
+            emotions = formatted_response['emotionPredictions'][0]['emotion']
+            dominant_emotion = max(emotions, key=emotions.get)
+            anger_score = emotions['anger']
+            disgust_score = emotions['disgust']
+            fear_score = emotions['fear']
+            joy_score = emotions['joy']
+            sadness_score = emotions['sadness']
+
     except requests.exceptions.RequestException as e:
         print(f"Request error: {e}") # Error log
 
-    return formatted_response
+    return {
+            'anger': anger_score,
+            'disgust': disgust_score,
+            'fear': fear_score,
+            'joy': joy_score,
+            'sadness': sadness_score,
+            'dominant_emotion': f'{dominant_emotion}'
+    }
