671bbde473aa07acaab7758bf4d899bc5e272fbb
Alejo <alejojpa2@gmail.com>
2025-03-01 12:10:39 -0500

    mywebscript was updated to dsiplay message error 400

--- static/mywebscript.js
+++ static/mywebscript.js
@@ -1,12 +1,18 @@
-let RunSentimentAnalysis = ()=>{
-    textToAnalyze = document.getElementById("textToAnalyze").value;
+let RunSentimentAnalysis = () => {
+    let textToAnalyze = document.getElementById("textToAnalyze").value;
 
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("system_response").innerHTML = xhttp.responseText;
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                document.getElementById("system_response").innerHTML = xhttp.responseText;
+            } else if (this.status == 400) {
+                // Handle error message when status is 400
+                let errorMessage = JSON.parse(xhttp.responseText).error; 
+                document.getElementById("system_response").innerHTML = `<span style="color: red;">${errorMessage}</span>`;
+            }
         }
     };
-    xhttp.open("GET", "emotionDetector?textToAnalyze"+"="+textToAnalyze, true);
+    xhttp.open("GET", "emotionDetector?textToAnalyze=" + encodeURIComponent(textToAnalyze), true);
     xhttp.send();
-}
+};
